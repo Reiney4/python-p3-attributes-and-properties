1b41d8dee247ce7383ee5e6e6f879cd407b54bcc
Valerie <valeriejeptoo4@gmail.com>
2023-08-23 10:26:01 +0300

    completed

--- lib/person.py
+++ lib/person.py
@@ -27,7 +27,7 @@ class Person:
         if isinstance (name, str) and (1 <=len(name)<= 25):
           self._name = name.title()
         else:
-            print("Name must be string under 25 characters.")
+            print("Name must be string between 1 and 25 characters.")
     def get_name(self):
         return self._name
     name =property(get_name,set_name)
